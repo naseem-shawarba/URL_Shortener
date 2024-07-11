18ab88fbafc5f37318f6d7cef1f98ada3430a70e
naseem-shawarba <naseem.shawarba@gmail.com>
2024-07-11 20:44:08 +0200

    Complete URL shortener application

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const ShortURL = require('./models/shortURL')
+const shortId = require('shortid')
+
 const app = express()
 
 mongoose.connect('mongodb://localhost/urlShortener', {
@@ -8,10 +11,27 @@ mongoose.connect('mongodb://localhost/urlShortener', {
 });
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) => {
-  res.send('Hello, world!');
-});
+  const shortURL = await ShortURL.find()
+  res.render('index', { shortURLs: shortURL })
+})
 
+app.post('/shortURLs', async (req, res) => {
+  await ShortURL.create({ full: req.body.fullURL })
+
+  res.redirect('/')
+})
+
+app.get('/:shortURL', async (req, res) => {
+  const shortURL = await ShortURL.findOne({ short: req.params.shortURL })
+  if (shortURL == null) return res.sendStatus(404)
+
+  shortURL.clicks++
+  shortURL.save()
+
+  res.redirect(shortURL.full)
+})
 
 app.listen(process.env.PORT || 5500);
